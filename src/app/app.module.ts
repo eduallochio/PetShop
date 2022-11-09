2fe3214176a16c53136a8e78f569fb47d6e6b974
Eduardo Jonais Silva Allochio <eduallochio2@outlook.com>
2022-11-09 17:11:42 -0300

    inicio do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPassowordPageComponent } from './reset-passoword-page/reset-passoword-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPassowordPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/reset-passoword-page/reset-passoword-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-passoword-page',
+  templateUrl: './reset-passoword-page.component.html',
+  styleUrls: ['./reset-passoword-page.component.css']
+})
+export class ResetPassowordPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
